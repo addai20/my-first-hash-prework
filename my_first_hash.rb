28c24bd684863e6565bf7c8395f330c93ef6cc71
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-18 01:20:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"name"=>"Benjamin", "height(inches)"=>70, "age"=>26}
+  my_hash = {
+    "name"=>"Benjamin", 
+    "height(inches)"=>70, 
+    "age"=>26
+  }
 
   
 end
@@ -11,7 +15,7 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
-  the_manifest = retrieval
+  the_manifest = shipping_manifest
 
 
 end
